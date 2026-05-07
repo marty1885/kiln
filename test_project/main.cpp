d5ec3a2b0d547dafe6ee6f0468ec286bb446f10f
Martin Chang <marty1885@users.noreply.github.com>
2026-05-07 16:34:37 +0800

    delete grabage

--- test_project/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-void say_hello();
-
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    say_hello();
-    return 0;
-}
--- test_project/my_lib.cpp
+++
@@ -1,5 +0,0 @@
-#include <iostream>
-
-void say_hello() {
-    std::cout << "Hello from my_lib!" << std::endl;
-}
